49039f1b75d23a71e8b24ffbef3db653c6ac9e3b
AbdullahCheema <2287153@brunel.ac.uk>
2023-11-16 21:33:44 +0000

    added file3
    nnn

---
+++ file3.js
@@ -0,0 +1,9 @@
+// Prompt user for their name
+var userName = prompt("What's your name?");
+
+// Display a greeting
+if (userName) {
+  alert("Hello, " + userName + "! Welcome to the world of JavaScript.");
+} else {
+  alert("Hello, stranger! Welcome to the world of JavaScript.");
+}
